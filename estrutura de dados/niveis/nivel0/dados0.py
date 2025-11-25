06736e2ad23b4f2f919412376bef2fe4ee5eee3b
Ricardo <ricotavares7719@gmail.com>
2025-11-25 11:16:14 -0300

    vamos pra cima

---
+++ estrutura de dados/niveis/nivel0/dados0.py
@@ -0,0 +1,61 @@
+#**********************PRÉ INCIANTE**************************
+#**********************PRÉ INICIANTE*************************
+#Exercicio 1 : Crie uma lista com 3 números e imprima o primeiro elemento. 
+lista=[-1,0,1,2,3]
+print(lista[0])#usando a indexação para determinar o elemento que retorne.
+#-----------------------------------------------------------------------------------------
+
+#Exercicio 2: Crie um array de inteiros com os valores [5, 10, 15] e imprima o último elemento. 
+numeros=(5,10,15)
+print(numeros[2])#Usando novamente a indexação para determinar o elemento que retorne.
+#------------------------------------------------------------------------------------------
+
+#Exercicio 3:Crie um dicionário com as chaves "nome" e "idade" e imprima o valor da chave "nome". 
+aluno={
+    "nome":"Ricardo",
+    "idade":48,
+}
+print(aluno["nome"])
+#-------------------------------------------------------------------------------------------
+
+#Exercício 4: Adicione o número 20 a uma lista vazia e imprima a lista. 
+lista=[]
+lista.append(20)#com o comando append inseririmos um elemento no final da lista.
+print(lista)
+#-------------------------------------------------------------------------------------------
+
+#Exercicio 5: Crie uma lista com 4 elementos e imprima o tamanho da lista usando len(). 
+frutas=["abacaxi","amora","abacate","ameixa"]
+frutas1=len(frutas)#o comando len verifica o tamanho da lista nesse caso usamos em strings também.
+print(frutas1)
+#-------------------------------------------------------------------------------------------
+
+#Exercicio 6: Crie uma lista com os números [1, 2, 3] e imprima o segundo elemento. 
+numeros=[1,2,3]
+print(numeros[1])#Aqui usamos a indexação novamente para verificar o elemento da posição desejada.
+#-------------------------------------------------------------------------------------------
+
+#Exercicio 7: Crie um dicionário com "animal": "cachorro" e "cor": "marrom" e imprima a cor. 
+animais={
+    "animal":"cachorro",
+    "cor":"marrom",
+}
+print(animais["cor"])#Aqui usamos a indexação novamente para verificar o elemento da posição desejada.
+#------------------------------------------------------------------------------------------
+
+#Exercicio 8: Crie um array de inteiros [2, 4, 6] e imprima o primeiro elemento. 
+numeros=(2,4,6)
+print(numeros[0])#Aqui usamos a indexação novamente para verificar o elemento da posição desejada.
+#-------------------------------------------------------------------------------------------
+
+#Exercicio 9:Crie uma lista com os elementos "a", "b", "c" e imprima o último. 
+letras=["a","b","c"]
+print(letras[2])#Aqui usamos a indexação novamente para verificar o elemento da posição desejada.
+#
+
+#Exercicio 10: Crie um dicionário com "marca": "Ford" e "ano": 2020 e imprima o ano. 
+carro={
+    "marca":"Ford",
+    "ano":2020,
+}
+print(carro["ano"])#Aqui usamos a indexação novamente para verificar o elemento da posição desejada.
---
+++ estrutura de dados/niveis/nivel1/dados1.py
@@ -0,0 +1,15 @@
+#*****************************INICIANTE**********************************************
+#Exercicio 1: Crie uma lista com números de 1 a 5 e adicione o número 6 ao final. 
+numeros=[1,2,3,4,5]
+numeros.append(6)#com o comando append inseririmos um elemento no final da lista.
+print(numeros)
+#---------------------------------------------------------------------------------
+
+#Exercicio 2: Crie um array de inteiros [2, 4, 6, 8] e substitua o valor da posição 2 por 10. 
+numeros=[2,4,6,8]
+numeros[2]=10
+print(numeros)
+#------------------------------------------------------------------------------------------
+
+
+
