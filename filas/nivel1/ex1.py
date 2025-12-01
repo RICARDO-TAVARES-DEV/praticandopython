2b28e640f2442ed9331eadf35329b13730d6d0f7
Ricardo <ricotavares7719@gmail.com>
2025-12-01 10:48:50 -0300

    vamos pra cima

---
+++ filas/nivel1/ex1.py
@@ -0,0 +1,5 @@
+#Crie uma fila de clientes e atenda o primeiro. # aqui ao falar que atende ele sai da fila
+clientes=["Ricardo","Roberto","Ronaldo","Romario"]
+print(clientes.pop(0))# então como o primeiro elemento ta saindo preciso remove-lo nesse caso usando o pop(0)e claro ele esta na primeira posição 
+print(clientes)#aqui ficou o restante da fila
+
---
+++ filas/nivel1/ex2.py
@@ -0,0 +1,5 @@
+#Crie uma fila de 5 números e remova dois primeiros. 
+numeros=[1,2,3,4,5]
+numeros.pop(0)
+numeros.pop(0)
+print(numeros)
